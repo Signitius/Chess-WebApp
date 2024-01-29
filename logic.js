321f0b55587503247f77dc0805f80d55aca2061e
ignitius kd <kasoigoi@gmail.com>
2024-01-29 22:33:20 +0300

    refactor to reduce spaghetti

--- logic.js
+++ logic.js
@@ -15,6 +15,17 @@ export function idTocodes(id){}
 function updateBoard(){
     let images=querySelectorAll("img");
     images.remove();
+    for (let entry of Object.entries(gameState.currentPosition)) updateType(entry);
+}
+
+function updateType(entry){
+    let imageSources=["wikipedia/blackRook.png","wikipedia/blackKnight.png","wikipedia/blackBishop.png",
+    "wikipedia/blackQueen.png","wikipedia/blackKing.png","wikipedia/blackPawhiteKnight.png",
+    "wikipedia/whitePawn.png","wikipedia/whiteRook.png","wikipedia/whiteKnight.png",
+    "wikipedia/whiteBishop.png","wikipedia/whiteQueen.png","wikipedia/whiteKing.png"]
+    for (let location of entry[1]){
+
+    }
 }
 function updateStatus(){}
 function checkStatus(){}
